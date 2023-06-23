7b3c39ce8e85aa2aea3c2f3fcf38d41421851518
spxf <spxf@Spxf.local>
2023-06-23 23:25:39 +0700

    add match

--- main.rs
+++ main.rs
@@ -5,6 +5,18 @@ fn main() {
     println!("{greeting}");
     //Tuple
 
+    // match, like switch case
+    fn ov_match() {
+        let condition = 1;
+
+        match condition {
+            1 => println!("Overview match success, condition is {condition}"),
+            _ => println!("Overview match success, condition is not founded")
+        }
+    }
+    ov_match();
+
+
     // function
     fn get_string() -> String {
         return String::from("Hello world!");
