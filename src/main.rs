3cf79f7b573006aa9dde9e82dffce89ad0ee40f3
spxf <sonphung.xf@gmail.com>
2023-07-12 13:58:03 +0700

    fix bugs

--- src/main.rs
+++ src/main.rs
@@ -13,37 +13,34 @@ fn main() {
     println!("true AND false is {}", true && false);
     println!("true OR false is {}", true || false);
     println!("NOT true is {}", !true);
-    
+
     println!("One million is written as {}", 1_000_000u32);
 
     // =================== assert_eq ===================
     // assert_eq if fail will throw an error and cancel the program
     // assert_eq!(6, 5); this code will broke prod
     assert_eq!(5, 5);
-    
+
     // array
     println!("Array ==================");
-    let arr1 = [1,2,3,4, 65];
+    let arr1 = [1, 2, 3, 4, 65];
     let arr2: [i32; 20] = [0; 20];
-    
+
     println!("Array 1 is {:?}", arr1);
     println!("Array 2 is {:#?}", arr2);
 
     println!("Array 1 length is {:?}", arr1.len());
     println!("Array 1 memory size is {}", mem::size_of_val(&arr1));
-    
+
     // looping
-    let looping_arr = [1,2,3,4];
+    let looping_arr = [1, 2, 3, 4];
     for i in 0..looping_arr.len() {
         println!("Index is {}", i);
         println!("Item is {}", looping_arr[i])
     }
-    
-    
-
 
     // =================== Slice ===================
-    let slice1 : &[i8] = &[0, 1, 2,3,4];
+    let slice1: &[i8] = &[0, 1, 2, 3, 4];
     println!("Slice 1 length is {}", slice1.len());
 
     // match, like switch case
@@ -53,19 +50,19 @@ fn main() {
         match condition {
             1 => println!("Overview match success, condition is 1"),
             2 => println!("Overview match success, condition is 2"),
-            _ => println!("Overview match success, condition is not founded")
+            _ => println!("Overview match success, condition is not founded"),
         }
     }
     ov_match();
     // Vector
-    let mut numbers_vec:Vec<i32> = Vec::new();
+    let mut numbers_vec: Vec<i32> = Vec::new();
     numbers_vec.push(10);
     numbers_vec.push(20);
     numbers_vec.push(30);
     numbers_vec.push(40);
     println!("Vec numbers first elements is {}", numbers_vec[0]);
 
-    // 
+    //
 
     // Function ===========
     fn get_greeting() {
@@ -75,21 +72,51 @@ fn main() {
     fn get_greeting_with_return() -> String {
         return String::from("Hello world!");
     }
-    println!("Funtion end with return statement : {}", get_greeting_with_return());
+    println!(
+        "Funtion end with return statement : {}",
+        get_greeting_with_return()
+    );
     fn get_greeting_without_return(str: String) -> String {
         str
     }
-    println!("Funtion can end without return statement : {}", get_greeting_without_return(String::from("Hello world")));
+    println!(
+        "Funtion can end without return statement : {}",
+        get_greeting_without_return(String::from("Hello world"))
+    );
 
-    // Struct
+    // Struct, trait .......
     struct Student {
-        name: String;
-    } 
-
-    pub trait year_only {
-        fn year
+        subject_name: String,
     }
 
-    
+    struct Programmer {
+        programming_language_name: String,
+    }
 
+    pub trait Learnable {
+        fn learn(&self) -> String;
+    }
+
+    impl Learnable for Student {
+        fn learn(&self) -> String {
+            self.subject_name.clone()
+        }
+    }
+
+    impl Learnable for Programmer {
+        fn learn(&self) -> String {
+            self.programming_language_name.clone()
+        }
+    }
+
+    let student: Student = Student {
+        subject_name: String::from("Math"),
+    };
+
+    let programmer: Programmer = Programmer {
+        programming_language_name: String::from("Rust"),
+    };
+
+    println!("{:#?}", student.learn());
+    println!("{:#?}", programmer.learn());
 }
